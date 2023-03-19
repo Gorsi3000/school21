e7cb78fd9c8c21d555aee8aea2d4f4033259d6f4
Page Moises <pagemois@ga-o2.msk.21-school.ru>
2023-03-19 20:51:38 +0300

    output

--- src/output.c
+++ src/output.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "output.h"
 
 typedef struct {
     int x;
     int y;
 } point;
 
-void output(point *out, int n) {
-    int **matrix = (int **)calloc(25, sizeof(int *));
-    for (int i = 0; i < 25; i++) {
-        matrix[i] = (int *)calloc(80, sizeof(int));
-    }
-    for (int i = 0; i < n; i++) {
-        int index_i = out[i].y;
-        int index_j = out[i].x;
-        matrix[index_i][index_j] = 1;
-    }
-
-    for (int i = 0; i < 25; i++) {
+void output_v2(double const array[]) {
+    for (double i = -1; i <= 1; i += (1.0 / ((25 - 1) / 2))) {
         for (int j = 0; j < 80; j++) {
-            matrix[i][j] == 1 ? printf("*") : printf(".");
+            if (array[j] > i && array[j] <= (i + 1.0 / 12.0)) {
+                printf("*");
+            } else {
+                printf(".");
+            }
         }
-        if (i+1 != 25)
-            printf("\n");
+        printf("\n");
     }
-
-    for (int i = 0; i < 25; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
 }
+
+
+//void output(point *out, int n) {
+//    int **matrix = (int **)calloc(25, sizeof(int *));
+//    for (int i = 0; i < 25; i++) {
+//        matrix[i] = (int *)calloc(80, sizeof(int));
+//    }
+//    for (int i = 0; i < n; i++) {
+//        int index_i = out[i].y;
+//        int index_j = out[i].x;
+//        matrix[index_i][index_j] = 1;
+//    }
+//
+//    for (int i = 0; i < 25; i++) {
+//        for (int j = 0; j < 80; j++) {
+//            matrix[i][j] == 1 ? printf("*") : printf(".");
+//        }
+//        if (i+1 != 25)
+//            printf("\n");
+//    }
+//
+//    for (int i = 0; i < 25; i++) {
+//        free(matrix[i]);
+//    }
+//    free(matrix);
+//}
