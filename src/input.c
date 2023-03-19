c0eb97b3396051599890acfd4ab760f488d36f1f
Page Moises <pagemois@ga-o2.msk.21-school.ru>
2023-03-19 17:33:06 +0300

    input

--- src/input.c
+++ src/input.c
@@ -11,7 +11,7 @@ void init_lexem(double value, lexema_type type, lexema **lex, int index);
 
 lexema ** input(void) {
     char *string;
-    lexema **lex = (lexema **)malloc(sizeof(lexema *));
+    lexema **lex = (lexema **)malloc(sizeof(lexema *) * SIZE);
     string = (char *)malloc(sizeof(char) * SIZE);
     if (string == NULL) {
         printf("Memory allocation error\n");
@@ -25,58 +25,50 @@ lexema ** input(void) {
     int index_lexema = 0;
     int n_index = 0;
     char number[10];
-    printf("%s\n", string);
     while (string[i] != '\0') {
         ch = string[i];
-        printf("%d: %c %d\n", i, ch, index_lexema);
         while (_isdigit(ch)) {
             number[n_index++] = ch;
             ch = string[i++];
             flag = 1;
-            printf("YE TUT \n");
         }
         if (flag) {
             printf("NUMBER ");
             double digit = atof(number);
             init_lexem(digit, operand, lex, index_lexema);
-            printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
             index_lexema++;
             flag = 0;
+            i--;
         }
 
         if (ch == 's') {
-            printf("______%c_______\n", string[i+1]);
             if (string[i+1] == 'i') {
                 printf("SINUS ");
                 init_lexem(0, sinus, lex, index_lexema);
-                printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
                 lex[index_lexema]->one_param = sin;
-                i += 2;
+                i += 1;
                 index_lexema++;
             }
             else {
                 printf("SQRT ");
                 init_lexem(0, sqrt_l, lex, index_lexema);
-                printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
                 lex[index_lexema]->one_param = sqrt;
-                i += 2;
+                i += 3;
                 index_lexema++;
             }
         }
         if (ch == 'l') {
             printf("LOG ");
             init_lexem(0, log_e, lex, index_lexema);
-            printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
             lex[index_lexema]->one_param = log;
-            i += 3;
+            i += 1;
             index_lexema++;
         }
         if (ch == 'c') {
-            char ch2 = string[i];
+            char ch2 = string[i+1];
             if (ch2 == 'o') {
                 printf("COSINUS ");
                 init_lexem(0, cosinus, lex, index_lexema);
-                printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
                 lex[index_lexema]->one_param = cos;
                 index_lexema++;
                 i += 2;
@@ -84,7 +76,6 @@ lexema ** input(void) {
             else {
                 printf("COTANGENS ");
                 init_lexem(0, cotangens, lex, index_lexema);
-                printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
                 lex[index_lexema]->one_param = ctg;
                 i += 1;
                 index_lexema++;
@@ -93,7 +84,6 @@ lexema ** input(void) {
         if (ch == 't') {
             printf("TANGENS ");
             init_lexem(0, tangens, lex, index_lexema);
-            printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
             lex[index_lexema]->one_param = tan;
             i++;
             index_lexema++;
@@ -101,55 +91,47 @@ lexema ** input(void) {
         if (ch == '(') {
             printf("BRACKET_OPEN ");
             init_lexem(0, bracket_open, lex, index_lexema);
-            printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
             index_lexema++;
         }
         if (ch == ')') {
-            printf("BRACKET CLOSE ");
+            printf("BRACKET_CLOSE ");
             init_lexem(0, bracket_close, lex, index_lexema);
-            printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
             index_lexema++;
         }
         if (ch == '+') {
             printf("PLUS ");
             init_lexem(0, plus, lex, index_lexema);
-            printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
             lex[index_lexema]->two_param = sum;
             index_lexema++;
         }
         if (ch == '-') {
-            if (_isdigit(string[i-1])) {
-                printf("BINARY MIN ");
-                init_lexem(0, min_binary, lex, index_lexema);
-                printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
-                lex[index_lexema]->two_param = binary_minus;
-                index_lexema++;
-            } else {
+            if (i == 0 || string[i-1] == ')') {
                 printf("UNARNIY MIN ");
                 init_lexem(0, min_unary, lex, index_lexema);
-                printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
                 lex[index_lexema]->one_param = unary_minus;
+                index_lexema++;
+            } else {
+                printf("BINARNIY MIN ");
+                init_lexem(0, min_binary, lex, index_lexema);
+                lex[index_lexema]->two_param = binary_minus;
                 index_lexema++;
             }
         }
         if (ch == '*') {
             printf("ZVEZDO4ka ");
             init_lexem(0, mul, lex, index_lexema);
-            printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
             lex[index_lexema]->two_param = multiply;
             index_lexema++;
         }
         if (ch == '/') {
             printf("DELENIE ");
             init_lexem(0, division, lex, index_lexema);
-            printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
             lex[index_lexema]->two_param = _division;
             index_lexema++;
         }
         if (ch == 'x') {
             printf("XXX ");
             init_lexem(0, X, lex, index_lexema);
-            printf("i = %d lexem_count = %d enum = %d\n", i, index_lexema, lex[index_lexema]->type);
             index_lexema++;
         }
         i++;
@@ -161,6 +143,7 @@ lexema ** input(void) {
 }
 
 void init_lexem(double value, lexema_type type, lexema **lex, int index) {
+
     lex[index] = (lexema *)malloc(sizeof(lexema));
     if (lex[index] == NULL) {
         printf("Memory allocation error\n");
